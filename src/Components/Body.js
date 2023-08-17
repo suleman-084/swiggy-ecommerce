35a44712febe642ec4e1283350edd3cff8385d80
mohammed suleman <sallusk084@gmail.com>
2023-08-17 16:34:55 +0530

    live api integrated and added the shimmer component

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,11 +1,47 @@
 import { ResCart } from "./ResCart";
-import { resList } from "../Utils/mockdata";
+import { useEffect, useState } from "react";
+import { Shimmer } from "./Shimmer";
+
 export const Body = () => {
+  const [restaurantList, setRestaurantList] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.98358648148027&lng=77.48813699930905&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    // console.log("data", data);
+    const parseData = await data.json();
+    // console.log("parsedata", parseData.data);
+    setRestaurantList(
+      parseData?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
+  console.log("restaurantList:", restaurantList);
+  if (restaurantList?.length === 0) {
+    return <Shimmer />;
+  }
   return (
     <div className="body">
-      <div className="search">search</div>
+      <div className="btn">
+        <button
+          className="top-res"
+          onClick={() => {
+            const filteredList = restaurantList.filter(
+              (res) => res.info?.avgRating > 4
+            );
+            setRestaurantList(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+
       <div className="res-container">
-        {resList.map((restaurant) => {
+        {restaurantList?.map((restaurant) => {
           return <ResCart key={restaurant.info.id} resData={restaurant} />;
         })}
         {/* <ResCart resData={resList[0]} />
---
+++ src/Components/Shimmer.js
@@ -0,0 +1,15 @@
+export const Shimmer = () => {
+  return (
+    <div className="shimmer-container">
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+    </div>
+  );
+};
