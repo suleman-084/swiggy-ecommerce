cbbc16a498694d569534ac4768c69b1f9b730d0d
mohammed suleman <sallusk084@gmail.com>
2023-08-21 19:43:15 +0530

    updated the about us page and fetched the api using setstate

---
+++ src/Components/DummyClass.js
@@ -0,0 +1,32 @@
+import React from "react";
+class DummyClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      count2: 2,
+    };
+  }
+  render() {
+    const { name, location, contact } = this.props;
+    const { count, count2 } = this.state;
+    return (
+      <div className="user-card-class">
+        <h1>count : {count}</h1>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Increasing Count
+        </button>
+        <h2>Name: {name}</h2>
+        <h3>Location: {location}</h3>
+        <h3>Contact: {contact}</h3>
+      </div>
+    );
+  }
+}
+export default DummyClass;
--- src/Components/UserClass.js
+++ src/Components/UserClass.js
@@ -3,28 +3,28 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
-      count2: 2,
+      userinfo: {
+        name: "dummyName",
+        location: "dummlocation",
+      },
     };
   }
+
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/akshaymarch7");
+    const parseData = await data.json();
+    this.setState({
+      userinfo: parseData,
+    });
+  }
+
   render() {
-    const { name, location, contact } = this.props;
-    const { count, count2 } = this.state;
+    const { name, location, avatar_url } = this.state.userinfo;
     return (
       <div className="user-card-class">
-        <h1>count : {count}</h1>
-        <button
-          onClick={() => {
-            this.setState({
-              count: this.state.count + 1,
-            });
-          }}
-        >
-          Increasing Count
-        </button>
+        <img className="abt-img" alt="git profile" src={avatar_url} />
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
-        <h3>Contact: {contact}</h3>
       </div>
     );
   }
